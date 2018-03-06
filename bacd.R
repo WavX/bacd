df8f77d9ea33dfedac8b5550c5cbbffec54124be
JM <jean.d.marchal@gmail.com>
2018-03-06 17:19:56 -0500

    Initial commit

---
+++ bacd.R
@@ -0,0 +1,13 @@
+#' bacd
+#'
+#' Wave file containing 20 bat echolocation calls from the Myotis genus
+#' @name bacd
+#' @docType data
+#' @description The 'bacd' dataset is an ongoing BAt Call Database project that aims to
+#' gather referenced echolocation calls of bat species around the globe.
+#' Fresh contributions to this database with new recordings are always welcome.
+#' For future contribution, the metadata listed in the 'bacd' dataframe must be provided
+#' along with the echolocation calls.
+#' @keywords bats, echolocation calls
+
+
